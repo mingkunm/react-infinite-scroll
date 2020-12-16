a5bd5f1efb072d979bbef75efc5437fc980eaaf2
mmk <mmk.ee.911@gmail.com>
2020-12-16 12:42:41 -0800

    new token

---
+++ src/env/index.js
@@ -0,0 +1,1 @@
+export const token = "token d08551c41332b7086762e74a5efb480b42239af1";
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,17 +1,24 @@
 import axios from "axios";
-const envToken;
-try {
-  import { token } from "../config";
-} catch (err) {
-  envToken = process.env.token;
-}
+import { token } from "../env";
+// const envToken;
+// try {
+//   import { token } from "../config";
+// } catch (err) {
+//   envToken = process.env.token;
+// }
+
+// const auth = axios.create({
+//   baseURL: "https://api.github.com/",
+// });
+
+// if (token) {
+//   auth.defaults.headers.common["Authorization"] = token || envToken;
+// }
 
 const auth = axios.create({
   baseURL: "https://api.github.com/",
 });
 
-if (token) {
-  auth.defaults.headers.common["Authorization"] = token || envToken;
-}
+auth.defaults.headers.common["Authorization"] = token;
 
 export default auth;
