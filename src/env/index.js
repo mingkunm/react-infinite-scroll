7163fa79a35578ef9fa6f2355edd837c7d1b2d28
mmk <mmk.ee.911@gmail.com>
2020-12-16 13:12:44 -0800

    fix env token

--- src/env/index.js
+++
@@ -1,1 +0,0 @@
-export const token = "token d08551c41332b7086762e74a5efb480b42239af1";
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,24 +1,25 @@
 import axios from "axios";
-import { token } from "../env";
-// const envToken;
-// try {
-//   import { token } from "../config";
-// } catch (err) {
-//   envToken = process.env.token;
-// }
+// import { token } from "../config";
 
-// const auth = axios.create({
-//   baseURL: "https://api.github.com/",
-// });
-
-// if (token) {
-//   auth.defaults.headers.common["Authorization"] = token || envToken;
-// }
+let envToken;
+let localToken;
+try {
+  const { token } = require("../config");
+  localToken = token;
+} catch (err) {
+  envToken = process.env.token;
+}
 
 const auth = axios.create({
   baseURL: "https://api.github.com/",
 });
 
-auth.defaults.headers.common["Authorization"] = token;
+auth.defaults.headers.common["Authorization"] = localToken || envToken;
+
+// const auth = axios.create({
+//   baseURL: "https://api.github.com/",
+// });
+
+// auth.defaults.headers.common["Authorization"] = token;
 
 export default auth;
